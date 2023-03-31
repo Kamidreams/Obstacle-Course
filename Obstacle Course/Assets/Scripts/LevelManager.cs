4f3c1d043df18523cb27247fb09517f547089241
Kamidreams <ahuante447@gmail.com>
2023-03-31 11:31:57 -0700

    Panels

--- Obstacle Course/Assets/Scripts/LevelManager.cs
+++ Obstacle Course/Assets/Scripts/LevelManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
+    public static LevelManager Instance;
+
+    public GameObject TitlePanel;
+    public GameObject WinnerPanel;
+    public GameObject GameOverPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,11 @@ public class LevelManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayButtonPressed()
+    {
+        Time.timeScale = 1;
+        TitlePanel.SetActive(false);
     }
 }
