099234a38cdf6922ba16d57cb05254af1e9c9625
Kamidreams <ahuante447@gmail.com>
2023-04-10 12:35:11 -0700

    Set Rotation for cam/player, Building maze, do colliders and collectables

--- Obstacle Course/Assets/Scripts/Forest scripts/Player.cs
+++ Obstacle Course/Assets/Scripts/Forest scripts/Player.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float _speed = 10f;
-    public float _RotationSpeed = 10f;
+    public float speed = 10f;
+    public float rotationSpeed = 10f;
+
+    private Rigidbody _playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 forwardInput = transform.forward * Input.GetAxis("Vertical");
+
+        _playerRb.AddForce(forwardInput * speed, ForceMode.Acceleration);
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+
+        //Movement();
     }
 
     void Movement()
     {
-        float xValue = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
-        float zValue = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        float xValue = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
         transform.Translate(xValue, 0f, zValue);
     }
